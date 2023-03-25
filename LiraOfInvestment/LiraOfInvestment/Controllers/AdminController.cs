be625b5f051d56102eea7ec9bac6619da61bd0ed
yusufkrtlss <krtlss.yusuf@gmail.com>
2023-03-25 06:05:07 +0300

    AdminLayout

---
+++ LiraOfInvestment/LiraOfInvestment/Controllers/AdminController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiraOfInvestment.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IProfileService _profileService;
+
+        public AdminController(IProfileService profileService)
+        {
+            _profileService = profileService;
+        }
+        [HttpGet("/Admin/Index")]
+        public IActionResult Index()
+        {
+            var values = _profileService.TGetList();
+            return View(values);
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+    }
+}
