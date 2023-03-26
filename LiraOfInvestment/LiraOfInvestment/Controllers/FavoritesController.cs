958677b79b624f1f18820abbdb71e75657298560
yusufkrtlss <krtlss.yusuf@gmail.com>
2023-03-26 06:16:23 +0300

    AdminLayout

---
+++ LiraOfInvestment/LiraOfInvestment/Controllers/FavoritesController.cs
@@ -0,0 +1,31 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace LiraOfInvestment.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        private IFavoriteService _favoriteService;
+
+        public FavoritesController(IFavoriteService favoriteService)
+        {
+            _favoriteService = favoriteService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route ("/Favorites/AddFavorite")]
+        public IActionResult AddFavorite(Favorite favorite)
+        {
+            _favoriteService.TAdd(favorite);
+            var jsonFavorite = JsonConvert.SerializeObject(favorite);
+            return Json(jsonFavorite);
+        }
+    }
+}
