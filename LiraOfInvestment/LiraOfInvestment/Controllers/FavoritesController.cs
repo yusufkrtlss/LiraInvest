59bdabfc4b35a5002c3b7e0648f7c921ee212871
yigitcancirpan <19070006058@stu.yasar.edu.tr>
2023-05-12 17:07:54 +0300

    favorite

--- LiraOfInvestment/LiraOfInvestment/Controllers/FavoritesController.cs
+++ LiraOfInvestment/LiraOfInvestment/Controllers/FavoritesController.cs
@@ -27,36 +27,35 @@ namespace LiraOfInvestment.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> AddFavoriteAsync(int id)
-        //{
-        //    var company = _profileService.TGetByID(id);
-        //    var user = await _userManager.FindByNameAsync(User.Identity.Name);
-        //    var model = new Favorite();
-        //    model.AppUserId=user.Id;
-        //    model.ProfileId = company.Id;
-        //    //var favs = _favoriteService.TGetList(company, user);
-        //    //_favoriteService.TAdd(model);
-        //    //model.UserId = user.Id;
-        //    //model.ProfileId = id;
-        //    var allFav = _favoriteService.TGetList().Where(x => x.ProfileId == id).ToList();
-        //    //foreach (var fav in allFav)
-        //    //{
-        //    //    if (fav.AppUserId == user.Id)
-        //    //    {
-        //    //        return Redirect("/Company/Index/" + id);
-        //    //    }
-        //    //    else
-        //    //    {
-        //    //        
-        //    //    }
-        //    //}
+        [HttpPost]
+        public async Task<IActionResult> AddFavoriteAsync(int id)
+        {
+            var company = _profileService.TGetByID(id);
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var model = new Favorite();
+            model.AppUserId = user.Id;
+            model.ProfileId = company.Id;
+
+            var allFav = _favoriteService.TGetList().Where(x => x.ProfileId == id).ToList();
+
+            _favoriteService.TAdd(model);
+            return Redirect("/Company/Index/" + id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveFavoriteAsync(int id)
+        {
+            var company = _profileService.TGetByID(id);
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var Fav=_favoriteService.TGetList().Where(x=>x.ProfileId == id&&x.AppUserId==user.Id).First();
+           
+            _favoriteService.TDelete(Fav);
 
 
-        //    _favoriteService.TAdd(model);
-        //    return Redirect("/Admin/Index/"+id);
-        //}
+           
+            return Redirect("/Company/Index/" + id);
+        }
 
-        
+
     }
 }
